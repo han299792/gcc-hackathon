3947a3ff86f07f35aa43a6d21e80121f6002a43e
dawnfire05 <dawnfire05@naver.com>
2024-05-12 02:07:52 +0900

    added global/places

--- post/models.py
+++ post/models.py
@@ -15,17 +15,17 @@ class mood_status(models.Model): #본인 기분
 
 class place(models.Model):
     place_name = models.CharField(max_length=30)
+    explanation = models.CharField(max_length=10000)
     lat = models.FloatField()
     lng = models.FloatField()
     place_tag = models.ForeignKey(place_tag, null = True, on_delete=models.SET_NULL)
     photo = models.ImageField(upload_to="", blank=True)
 
-
 class post(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    place_id = models.ForeignKey(place, on_delete=models.CASCADE)
-    created_date =models.DateTimeField(timezone.now)
-    updated_date = models.DateTimeField(null=True)
+    place_id = models.ForeignKey(place, related_name='posts', on_delete=models.CASCADE)
+    created_date =models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     mood_status = models.ForeignKey(mood_status, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=100000)
--- post/urls.py
+++ post/urls.py
@@ -3,9 +3,11 @@ from . import views
 from rest_framework import urls
 
 urlpatterns =[
-    path('posts/<int:pk>/', views.posts.as_view(), name='posts'),
     path('posts/', views.posts.as_view(), name='posts'),
-    path('calender/<int:year>/<int:month>/', views.PostsLastMonthAPIView.as_view(), name='year-to-month')
-    path('places/', views.place_get_in_map),
-    path('places/<int:pk>/', views.place_get_in_spot)
+    path('posts/<int:pk>/', views.posts.as_view(), name='posts'),
+    #path('calender/<int:year>/<int:month>/', views.PostsLastMonthAPIView.as_view(), name='year-to-month'),
+    path('personal/places/', views.place_get_in_map),
+    path('personal/places/<int:pk>/', views.place_get_in_spot),
+    path('global/places/', views.global_place_get),
+    path('global/places/<int:pk>/', views.global_place_get_detail)
 ]
