2edbffb68708deef1b15161ff538dee070ce2c1f
hansemin <han299792@gmail.com>
2024-05-12 02:24:04 +0900

    111

--- post/views.py
+++ post/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework import status
 from datetime import datetime, timedelta
-import json, string
+
 
 
 
