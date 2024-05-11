2080af57742dba8df2e6fe8387a9538d784f5155
hansemin <han299792@gmail.com>
2024-05-11 17:00:28 +0900

    ✅ user update

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,9 @@ from . import views
 from rest_framework import urls
 
 urlpatterns =[
-    path('signup/', views.UserCreate.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
+    #회원가입 화면
+    path('signup/',views.RegisterAPIView.as_view()),
+    #로그인 화면
+    path('auth/',views.AuthAPIView.as_view()),
+    #path('api-auth/', include('rest_framework.urls')),
  ]
